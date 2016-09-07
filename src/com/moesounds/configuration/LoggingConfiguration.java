83a7f884413f340f540ee213418f6fd55766ea8e
NYPD <nypd54@outlook.com>
2016-09-07 15:26:37 -0400

    Quick console logger, still need to hook up

--- src/com/moesounds/configuration/LoggingConfiguration.java
+++ src/com/moesounds/configuration/LoggingConfiguration.java
@@ -1,8 +1,24 @@
 package com.moesounds.configuration;
 
+import java.nio.charset.Charset;
+
+import org.apache.logging.log4j.core.appender.ConsoleAppender;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LoggingConfiguration {
 
+    @Bean
+    public ConsoleAppender consoleAppender() {
+
+        PatternLayout patternLayout = PatternLayout.newBuilder()
+                .withPattern("%-5p (%F:%M():%L) - %m%n")
+                .withCharset(Charset.defaultCharset())
+                .build();
+
+        ConsoleAppender appender = ConsoleAppender.createDefaultAppenderForLayout(patternLayout);
+        return appender;
+    }
 }
